5d56abba7a325a0b22d6b935c2c76b32d8701438
Yuriy Nikogosyan <ura.nikogosuan@gmail.com>
2026-01-09 19:46:45 +0500

    Готово

--- src/main/java/ru/nikogosyan/MySecondTestAppSpringBoot/service/ModifyOperationNameRequestService.java
+++ src/main/java/ru/nikogosyan/MySecondTestAppSpringBoot/service/ModifyOperationNameRequestService.java
@@ -1,5 +1,6 @@
 package ru.nikogosyan.MySecondTestAppSpringBoot.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -8,11 +9,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.nikogosyan.MySecondTestAppSpringBoot.model.Request;
 
+@Slf4j
 @Service
+//@Qualifier("ModifyOperationNameRequestService")
 public class ModifyOperationNameRequestService implements ModifyRequestService{
     @Override
     public void modify(Request request){
 //        request.setSystemName("Service 1");
+
+        log.info("Отправка запроса в Сервис 2...");
 
         HttpEntity<Request> httpEntity = new HttpEntity<>(request);
 
